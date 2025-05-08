8e71753a77e05847c91336792cb22a04ec85f7f7
princess-mikus <kiankocasaubon01@gmail.com>
2025-05-08 13:34:29 +0200

    Only TLS and db volume to do

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wordpress;
-CREATE USER 'wpuser'@'%' IDENTIFIED BY 'password';
+CREATE USER wpuser@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'wpuser'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
