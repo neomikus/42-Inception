c208dee0e135f6e487e0475a7725eac62f78ec40
princess-mikus <kiankocasaubon01@gmail.com>
2025-05-15 11:35:42 +0000

    Wordpress didn't push?

--- srcs/requirements/wordpress/wp-install.sh
+++ srcs/requirements/wordpress/wp-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#sleep 10
+#sleep 5
 cd /var/www/html
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -7,3 +7,5 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+php-fpm83 "-F"
