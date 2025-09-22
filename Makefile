38bec26f8c62efc97a9ab35bd0381e6d628cc712
princess-mikus <kiankocasaubon01@gmail.com>
2025-09-22 18:02:58 +0000

    Wordpress CSS not working

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-DOCKER_COMPOSE	:= srcs/docker-compose.yml
+DOCKER_COMPOSE	= srcs/docker-compose.yml
 
-SECRETS_DIR	:= secrets
+SECRETS_DIR	= secrets
 
-SSL_CERT	:= $(SECRETS_DIR)/self-signed.crt
+SSL_CERT	= $(SECRETS_DIR)/self-signed.crt
 
+DATA_DIR	= /home/fcasaubo/data
+DATABASE	= $(DATA_DIR)/database
+SITE		= $(DATA_DIR)/site
 
 all: $(SSL_CERT) build up
 
@@ -11,11 +14,26 @@ all: $(SSL_CERT) build up
 $(SSL_CERT):
 	sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $(SECRETS_DIR)/self-signed.key -out $(SSL_CERT) -subj "/C=ES/ST=Bizkaia/L=Urduliz/O=42Urduliz/CN=localhost"
 
-build:
+build: $(DATABASE) $(SITE)
 	sudo docker compose -f $(DOCKER_COMPOSE) build
+
+$(DATABASE): $(DATA_DIR)
+	mkdir -p $@
+
+$(SITE): $(DATA_DIR)
+	mkdir -p $@
+
+$(DATA_DIR):
+	mkdir -p $@
 
 up:
 	sudo docker compose -f $(DOCKER_COMPOSE) up -d
 
 down: 
 	sudo docker compose -f $(DOCKER_COMPOSE) down
+
+clean: down
+	sudo docker volume prune
+	sudo rm -rf $(DATA_DIR)/*
+
+re: clean all
--- srcs/requirements/wordpress/wp-install.sh
+++ srcs/requirements/wordpress/wp-install.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 #sleep 5
-cd /var/www/html
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
-wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+if ! [ -e /var/www/html/wp-config.php ]; then
+    cd /var/www/html
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+    wp core download --allow-root
+    wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+    wp core install --url=fcasaubo.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+    wp theme install twentysixteen --activate
+else
+    echo "Wordpress already installed"
+fi
 
 php-fpm83 "-F"
