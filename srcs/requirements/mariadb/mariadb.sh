c4bb24f0b1d7e508168dd211e454fcdd96abcc64
princess-mikus <kiankocasaubon01@gmail.com>
2025-10-01 10:36:20 +0000

    Secrets fully working

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+dbname=$(cut -d '\n' -f2 /run/secrets/db-user)
+dbpwd=$(cut -d '\n' -f2 /run/secrets/db-password)
+
+cat <<EOF > /etc/mysql/init.sql
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE OR REPLACE USER '$dbname'@'%' IDENTIFIED BY '$dbpwd';
+GRANT ALL PRIVILEGES ON *.* TO '$dbname'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+mysql_install_db --ldata=/var/lib/mysql
 exec mysqld
--- srcs/requirements/wordpress/wp-install.sh
+++ srcs/requirements/wordpress/wp-install.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-source /run/secrets/wp-admin-password
-source /run/secrets/wp-admin-user
-source /run/secrets/wp-user-password
-source /run/secrets/db-user
-source /run/secrets/db-password
-
 username=$WP_USERNAME
-userpwd=$WP_USERPWD
-adminuser=$WP_ADMINUSER
-adminpwd=$WP_ADMINPWD
-dbname=$DB_USERNAME
-dbpwd=$DB_PWD
+userpwd=$(cut -d '\n' -f2 /run/secrets/wp-user-password)
+adminuser=$(cut -d '\n' -f2 /run/secrets/wp-admin-user)
+adminpwd=$(cut -d '\n' -f2 /run/secrets/wp-admin-password)
+dbname=$(cut -d '\n' -f2 /run/secrets/db-user)
+dbpwd=$(cut -d '\n' -f2 /run/secrets/db-password)
 
 if ! [ -e /var/www/html/wp-config.php ]; then
     cd /var/www/html
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
+    rm -rf /var/www/html/*
+    if ! [ -e /usr/local/bin/wp ]; then
+        wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+        chmod +x wp-cli.phar
+        mv wp-cli.phar /usr/local/bin/wp
+    fi
     wp core download --allow-root
     wp config create --dbname=wordpress --dbuser=$dbname --dbpass=$dbpwd --dbhost=mariadb --allow-root
     wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$adminuser --admin_password=$adminpwd --admin_email=$adminuser@example.com --allow-root
